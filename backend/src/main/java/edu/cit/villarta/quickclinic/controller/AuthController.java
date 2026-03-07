edcece6cd6183ef3bb3e5cba5163d1aa6833d414
hetorydetorydak <johnhextor312@gmail.com>
2026-03-07 21:45:19 +0800

    chore(backend): allowed localhost:3000 to access the backend

--- backend/src/main/java/edu/cit/villarta/quickclinic/controller/AuthController.java
+++ backend/src/main/java/edu/cit/villarta/quickclinic/controller/AuthController.java
@@ -9,6 +9,7 @@ import edu.cit.villarta.quickclinic.dto.RegisterRequest;
 import edu.cit.villarta.quickclinic.model.User;
 import edu.cit.villarta.quickclinic.service.AuthService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
