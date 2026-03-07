d06d302af8d2ed5a98c636c41f5ffd9ff6ba2ff0
hetorydetorydak <johnhextor312@gmail.com>
2026-03-07 09:38:22 +0800

    setup(backend): initialize backend, and added essential configuration

---
+++ backend/src/main/java/edu/cit/villarta/quickclinic/QuickclinicApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.villarta.quickclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickclinicApplication.class, args);
+	}
+
+}
