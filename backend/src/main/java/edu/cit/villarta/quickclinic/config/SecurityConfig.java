f7e760d04db52b88358da8a495dc50dd88766a3d
hetorydetorydak <johnhextor312@gmail.com>
2026-03-07 20:00:12 +0800

    fix(backend): added Security Filter Chain to disable csrf for APIs

--- backend/src/main/java/edu/cit/villarta/quickclinic/config/SecurityConfig.java
+++ backend/src/main/java/edu/cit/villarta/quickclinic/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package edu.cit.villarta.quickclinic.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -9,8 +11,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig {
 
     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable()) // disable CSRF for APIs
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll() // allow register & login
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
 }
